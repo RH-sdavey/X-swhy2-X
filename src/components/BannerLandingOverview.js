88d67a62cdc57b0cd39e46aee8305e3299dd6f8b
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-07-12 12:29:09 +0200

    Create BannerLandingOverview.js

---
+++ src/components/BannerLandingOverview.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BannerLandingOverview = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Sean</h1>
+            </header>
+            <div className="content">
+                <p>So what do you want to be?<br />
+                Description here</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerLandingOverview
