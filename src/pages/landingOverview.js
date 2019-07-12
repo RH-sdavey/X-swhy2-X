1bb9666866b14180f0d7edb74b007c0d31f2ae23
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-07-12 12:29:51 +0200

    Update landingOverview.js

--- src/pages/landingOverview.js
+++ src/pages/landingOverview.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
-import BannerLandingSean from '../components/BannerLandingSean'
+import BannerLandingOverview from '../components/BannerLandingOverview'
 
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
