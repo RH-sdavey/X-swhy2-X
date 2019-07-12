8a6138ff6b6c098c58d49c8a071dcc4f0e348804
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-07-12 11:55:56 +0200

    Update and rename BannerLanding.js to BannerLandingSean.js

--- src/components/BannerLandingSean.js
+++ src/components/BannerLandingSean.js
@@ -4,11 +4,11 @@ const BannerLanding = (props) => (
     <section id="banner" className="style2">
         <div className="inner">
             <header className="major">
-                <h1>Landing</h1>
+                <h1>Sean</h1>
             </header>
             <div className="content">
-                <p>Lorem ipsum dolor sit amet nullam consequat<br />
-                sed veroeros. tempus adipiscing nulla.</p>
+                <p>Sean is actually two guys, called Sean<br />
+                and we want to help you work in IT.</p>
             </div>
         </div>
     </section>
