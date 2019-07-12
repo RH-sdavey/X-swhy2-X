bc51002ae2d053798a9d6ff0fc932e1a654db007
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-07-12 10:15:25 +0200

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,13 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Hi, my name is Sean, how can I help?</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>Sean is actually two guys...both called Sean,<br />
+                and we want to help you change your life, for free.</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#one" className="button next scrolly">Tell me more!</a></li>
                 </ul>
             </div>
         </div>
