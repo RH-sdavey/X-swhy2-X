404688b03ebfdb58c686f14a41282ef16ea21b52
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-07-12 15:36:16 +0200

    Create sean1.js

---
+++ src/pages/sean1.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+
+import sean from '../assets/images/sean.jpg'
+
+const Generic = (props) => (
+    <Layout>
+        <Helmet>
+            <title>Sean</title>
+            <meta name="description" content="Sean1 page" />
+        </Helmet>
+
+        <div id="main" className="alt">
+            <section id="one">
+                <div className="inner">
+                    <header className="major">
+                        <h1>Sean</h1>
+                    </header>
+                    <span className="image main"><img src={sean} alt="" /></span>
+                    <p>Sean bio here</p>
+                </div>
+            </section>
+        </div>
+
+    </Layout>
+)
+
+export default Generic
