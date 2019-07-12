ea055afc74700c38e53515a91370a644afefe9c1
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-07-12 12:02:21 +0200

    Create BannerLandingSean.js

---
+++ src/components/BannerLandingSean.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BannerLanding = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Sean</h1>
+            </header>
+            <div className="content">
+                <p>Sean is actually two guys, called Sean<br />
+                and we want to help you work in IT.</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerLandingSean
