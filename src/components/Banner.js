ef04af23f197c5a776a2db84b016bf803731d57e
Sean <40166737+Salloweezy@users.noreply.github.com>
2019-07-17 22:38:58 +0200

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,14 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Sean will help you</h1>
+                <h1>Sean Will Help You!</h1>
             </header>
             <div className="content">
                 <p>Sean is actually two guys...both called Sean,<br />
-                and we want to help you change your career and your life.</p>
+                & we want to help you change your career as well as your life.</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Tell me more!</a></li>
+                    <li><a href="#one" 
+                   className="button next scrolly">Click here for more</a></li>
                 </ul>
             </div>
         </div>
