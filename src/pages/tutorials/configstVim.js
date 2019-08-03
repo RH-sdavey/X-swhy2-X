d04b6b0edc8b13efa5233eb4c5cbdf3b558075bc
Sean Davey <sean.davey@tieto.com>
2019-08-03 10:48:04 +0200

    Vim 3

--- src/pages/tutorials/configstVim.js
+++ src/pages/tutorials/configstVim.js
@@ -18,13 +18,18 @@ const Generic = (props) => (
                         <h1>How to configure Splits and Tabs in Vim</h1>
                     </header>
                     <span className="image maintutorial"><img src={swhyvim} alt="" /></span>
-                    <div className="col-12">
-                      <h3>Sometimes you need to open multiple files in VIM and quickly move between them, rather than opening, editing, closing, reopening etc. Lets learn how,</h3>
-                      <h4>Tabs in VIM</h4>
+                    <div className="grid-wrapper">
+                      <div className="col-12">
+                        <h3>Sometimes you need to open multiple files in VIM and quickly move between them, rather than opening, editing, closing, reopening etc. Lets learn how,</h3>
+                        <h4>Tabs in VIM</h4>
+                      </div>
                       <div className="col-6">
                         <p>Open any file in the VIM editor</p>
                         <pre><code>vim /etc/hosts</code></pre>
-
+                      </div>
+                      <div className="col-6">
+                        <p>Image here</p>
+                        <span className="image fit"><img src={swhyvim} alt="" /></span>
                       </div>
                     </div>
                 </div>
